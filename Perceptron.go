d227d472fa7e54d0a4c35d879ec5a72504c20619
NathanielForde <nathaniel.forde@gmail.com>
2017-04-09 09:49:09 +0100

    Tidied and commented

--- Perceptron.go
+++ Perceptron.go
@@ -26,7 +26,8 @@ func readLines(path string) ([]string, error) {
 	return lines, scanner.Err()
 }
 
-func Round(val float64, roundOn float64, places int) (newVal float64) {
+///////// Rounding function for readibility
+func round(val float64, roundOn float64, places int) (newVal float64) {
 	var round float64
 	pow := math.Pow(10, float64(places))
 	digit := pow * val
@@ -40,6 +41,7 @@ func Round(val float64, roundOn float64, places int) (newVal float64) {
 	return
 }
 
+//////////// Classificiation function based of Boolean
 func binary(x bool) float64 {
 	if x {
 		return -1.0
@@ -47,17 +49,20 @@ func binary(x bool) float64 {
 	return 1.0
 }
 
+//////////////// Function for updating the weights based on observed deltas and learning rate
+
 func update(input []float64, delta float64, weights []float64, rate float64) []float64 {
 	for i := 1; i < len(weights); i++ {
 		//fmt.Println("Input", weights[i], "delta", delta, "rate", rate, "factor", input[i])
-		weights[i] = Round(weights[i], .5, 3) + delta*rate*input[i]
+		weights[i] = round(weights[i], .5, 3) + delta*rate*input[i]
 		//fmt.Println(weights[i])
 	}
 	//fmt.Println(weights[0], delta, rate)
-	weights[0] = Round(weights[0], .5, 3) + delta*rate
+	weights[0] = round(weights[0], .5, 3) + delta*rate
 	return weights
 }
 
+///////////////// Dot product function
 func dotproduct(input []float64, weights []float64) float64 {
 	sum := 0.0
 	for i := 0; i < len(input); i++ {
@@ -66,14 +71,7 @@ func dotproduct(input []float64, weights []float64) float64 {
 	return sum
 }
 
-func predict(input []float64, weights []float64) float64 {
-	sum := 0.0
-	for i := 0; i < len(input); i++ {
-		sum = sum + (input[i] * weights[i])
-	}
-	return heaviside(sum)
-}
-
+///////// Heaviside activation function
 func heaviside(x float64) float64 {
 	if x >= 0.0 {
 		return 1.0
@@ -81,17 +79,14 @@ func heaviside(x float64) float64 {
 	return -1.0
 }
 
-func sumvector(x []float64) float64 {
-	sum := 0.0
-	for i := 0; i <= len(x); i++ {
-		fmt.Println(x[i])
-		if x[i] == 1.0 {
-			sum = sum + 1
-		}
-	}
-	return sum
+//////////// Prediction function combining dotproduct and heaviside
+func predict(input []float64, weights []float64) float64 {
+	sum := dotproduct(input, weights)
+	return heaviside(sum)
 }
 
+//////////// Process function looping over each observation row-vector and updating the weights
+///////////  Returns new weights and error count for each prediction based on the observed values.
 func process(inputs [][]float64, weights []float64, rate float64) ([]float64, int) {
 	errorcount := 0
 	fmt.Println(weights)
@@ -100,12 +95,12 @@ func process(inputs [][]float64, weights []float64, rate float64) ([]float64, in
 		target := inputs[i][len(inputs[i])-1]
 		//fmt.Println("Target", i, ":", target)
 		prediction := predict(inputs[i][0:len(inputs[i])-1], weights)
-		dotprod := dotproduct(inputs[i][0:len(inputs[i])-1], weights)
+		//dotprod := dotproduct(inputs[i][0:len(inputs[i])-1], weights)
 		//fmt.Println("Prediction", i, ":", prediction)
 		delta := target - prediction
 		//fmt.Println("Target", i, ":", inputs[i][len(inputs[i])-1], "Delta", delta, "Dotproduct", dotprod, "Prediction", predict(inputs[i][0:len(inputs[i])-2], weights))
 		weights = update(inputs[i], delta, weights, rate)
-		fmt.Println("Round", i, "inputs", inputs[i], ":", "Weights", weights, "dot", dotprod, "target", target)
+		//fmt.Println("Round", i, "inputs", inputs[i], ":", "Weights", weights, "dot", dotprod, "target", target)
 		if delta != 0.0 {
 			errorcount = errorcount + 1
 		}
@@ -127,64 +122,43 @@ func perceptron(inputs [][]float64, weights []float64, rate float64, iterations 
 ////////// Each element of the slice is a string like: "5.9,3.0,5.1,1.8,Iris-virginica"
 
 func main() {
-
+	////////Loading in the iris data as a slice of row strings
 	iris, err := readLines("/Users/nathanielforde/Desktop/Data Science/Mathematical Probability/iris.data.csv")
 	if err != nil {
 		return
 	}
 
 	str := iris
-	////////////   We now extract the various row elements initially as strings, from the row-string
-	////////////  to force the values into numeric format.
-	inputs := make([][]float64, 100)
-	//var target []float64
+	//fmt.Println(str[1])
 
-	for i := 0; i < 100; i++ {
+	////////////   We now extract the various row elements initially as strings,
+	///////////    from the row-string ( e.g from  "5.9,3.0,5.1,1.8,Iris-virginica"), we extrace "5.9" and convert
+	////////////   to force the values into numeric format. We then reconstruct the row
+	////////////   and  put the details into a slice of slices called
+	///////////   inputs. We only need the first hundred observations of iris data. We will use a perceptron to
+	///////////   distinguish the Iris-virginica and Iris-setosa based on Sepal and Petal length.
+
+	inputs := make([][]float64, 100) ////// Declaring the empty slice of slices. Our design matrix.
+
+	for i := 0; i < 100; i++ { /////// pull out and convert each substring into a number
 		var input []float64
-		p, _ := strconv.ParseFloat(strings.Split(string(str[i]), ",")[0], 64)
+		p, _ := strconv.ParseFloat(strings.Split(string(str[i]), ",")[0], 64) ///// Sepal Length
 		//q, _ := strconv.ParseFloat(strings.Split(string(str[i]), ",")[1], 64)
-		r, _ := strconv.ParseFloat(strings.Split(string(str[i]), ",")[2], 64)
+		r, _ := strconv.ParseFloat(strings.Split(string(str[i]), ",")[2], 64) ///// Petal Length
 		//s, _ := strconv.ParseFloat(strings.Split(string(str[i]), ",")[3], 64)
 		t := binary(strings.Split(string(str[i]), ",")[4] == "Iris-setosa")
-		input = append(input, 1.0)
+		input = append(input, 1.0) /////// Recreating our row vectors for each observation
 		input = append(input, p)
 		//input = append(input, q)
 		input = append(input, r)
 		//input = append(input, s)
 		input = append(input, t)
-		inputs[i] = input
+		inputs[i] = input /////// Defining each row-vector observation (e.g [1, 5.9, 5.1, -1])
+		////// as an instance of our design matrix  ["intercept column", "sepal length", "petal length", target]
 	}
-	var a = []float64{1, -0.4, 0.3, 1}
-	var b = []float64{1, -0.3, -0.1, 1}
-	var c = []float64{1, -0.2, 0.4, 1}
-	var d = []float64{1, -0.1, 0.1, 1}
-	var e = []float64{1, 0.1, -0.5, -1}
-	var f = []float64{1, 0.2, -0.9, -1}
-	var g = []float64{-0.2, -0.26, 1.04}
-	var h = []float64{1, 4.6, 1.5}
-	var w = []float64{0, 0, 0}
-	//var c float64 = .1
-	//	var d float64 = .3
-	multi := make([][]float64, 8)
-	multi[0] = a
-	multi[1] = b
-	multi[2] = c
-	multi[3] = d
-	multi[4] = e
-	multi[5] = f
-	multi[6] = g
-	multi[7] = h
 
-	//fmt.Println(multi[1][len(w)+1] - heaviside(multi[1][0:len(w)], w))
-	//fmt.Println(dotproduct(a[0:len(w)], w))
-	//fmt.Println(heaviside(multi[1][0:len(w)], w))
-	//fmt.Println(multi[1][0:len(w)])
-	//fmt.Println(update(multi[1], 2, update(multi[0], -1, w, .1), .1))
-	//fmt.Println(len(str))
-	//fmt.Println(update(multi[1], 0.0, w, .01))
-	//fmt.Println(process(inputs, w, .01))
+	var w = []float64{0, 0, 0} ///////// initialising our weights
+
 	fmt.Println(perceptron(inputs, w, .1, 10))
-	//fmt.Println(multi[1][0:len(multi[1])-2], w)
-	//fmt.Println(inputs[3][0 : len(inputs[3])-1])
-	//fmt.Println(dotproduct(inputs[3][0:len(inputs[3])-1], g))
+
 }
